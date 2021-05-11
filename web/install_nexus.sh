87f8967d6fa17fd8e54ea28637618d34c1d24f9e
Zabasux <arnaud.degez@neo-soft.fr>
2021-05-11 10:46:30 +0200

    install serveur nexus #2

---
+++ web/install_nexus.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+set -e #En cas de code de retour non zero, arrêter le script
+
+# Alias jenkins
+
+# Variable de mise en forme
+RED='\033[0;31m'	# Red Color
+YELLOW='\033[0;33m'	# Yellow Color
+GREEN='\033[0;32m'	# Grean Color
+NC='\033[0m' 		# No Color
+
+
+##Fonction: 
+
+#Installation des packages sous forme de fonction avec vérification si le package est installé
+install_package() {
+    PACKAGE="$1"
+    if ! dpkg -l |grep --quiet "^ii.$PACKAGE"; then
+        apt install -y "$PACKAGE"
+    fi
+}
+
+##Main
+
+#mise à jour et installation openjdk et unzip
+apt-get update
+install_package "openjdk-8-jdk"
+install_package "unzip"
+
+
+## Récupération de la dernière version gradle
+
+VERSION=7.0
+wget https://downloads.gradle-dn.com/distributions/gradle-${VERSION}-bin.zip -P /tmp
+
+unzip -d /opt/gradle /tmp/gradle-${VERSION}-bin.zip
+
+# Faire pointer le lien vers la dernière version de gradle
+
+ln -s /opt/gradle/gradle-${VERSION} /opt/gradle/latest
+
+# Ajout de gradle au PATH
+
+touch /etc/profile.d/gradle.sh
+
+echo "export PATH=/opt/gradle/latest/bin:${PATH}" > /etc/profile.d/gradle.sh
+
+chmod +x /etc/profile.d/gradle.sh
+
+source /etc/profile.d/gradle.sh
+
+useradd -M -d /opt/nexus -s /bin/bash -r nexus
+echo "nexus   ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/nexus
+
+wget https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.29.2-02-unix.tar.gz
+
+mkdir /opt/nexus
+
+tar xzf nexus-3.29.2-02-unix.tar.gz -C /opt/nexus --strip-components=1
+
+chown -R nexus: /opt/nexus
+
+sed -i 's/#run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
+
+## On modifie la configuration pour qu'elle fonctionne
+sudo sed -i 's/..\/sonatype-work/.\/sonatype-work/g' /opt/nexus/bin/nexus.vmoptions
+sudo sed -i 's/2073/1024/g' /opt/nexus/bin/nexus.vmoptions
+
+sudo -u nexus /opt/nexus/bin/nexus start
+
+
+# Service
+
+cat > /etc/systemd/system/nexus.service << 'EOL'
+[Unit]
+Description=nexus service
+After=network.target
+[Service]
+Type=forking
+LimitNOFILE=65536
+ExecStart=/opt/nexus/bin/nexus start
+ExecStop=/opt/nexus/bin/nexus stop
+User=nexus
+Restart=on-abort
+[Install]
+WantedBy=multi-user.target
+EOL
+
+/opt/nexus/bin/nexus stop
+
+systemctl daemon-reload
+
+systemctl enable --now nexus.service
+
+ufw default allow outgoing
+
+ufw allow 8081
+ufw allow ssh
+
+ufw --force enable
+
+# Affiche le mot de passe
+
+sleep 30s #il faut attendre que le fichier se crée.
+
+echo 'Mot de passe admin \n'
+cat /opt/nexus/sonatype-work/nexus3/admin.password
+echo '\n\n'
+
+#cp /home/project/alex/gradle.properties /home/vagrant/.gradle/
